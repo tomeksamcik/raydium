e2f59a8cd0b947b0d1118235ea6e09290eb6183d
Tomek Samcik <tomek.samcik@whitehatgaming.com>
2025-06-14 23:49:17 +0200

    Remove Service annotation from interface

--- src/main/java/pro/samcik/raydium/service/RangeChecker.java
+++ src/main/java/pro/samcik/raydium/service/RangeChecker.java
@@ -1,9 +1,7 @@
 package pro.samcik.raydium.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface RangeChecker {
 
     void checkRange() throws JsonProcessingException;
